f625b1077e4b9615b6319289157bd31703c2bc39
Grzegoworz <87151830+Grzegoworz@users.noreply.github.com>
2021-11-15 19:25:10 +0100

    Yiff me daddy uwu~

---
+++ Classedelcazzo.java
@@ -0,0 +1,30 @@
+package com.cum;
+
+public class Classedelcazzo {
+    int dadowo = 1;
+    int daduwu;
+
+    String Striwinga;
+    String nuts;
+
+    public void setDadowo(int dadowo) {
+        this.dadowo = dadowo;
+        daduwu = dadowo + 1;
+    }
+
+    public int getDadowo() {
+        return daduwu;
+    }
+
+    public String getStriwinga() {
+        return Striwinga;
+    }
+
+    public void setStriwinga(String striwinga) {
+        Striwinga = striwinga;
+    }
+
+    public void setNuts(String nuts) {
+        this.nuts = nuts;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.cum;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Classedelcazzo Classedelcazzo = new Classedelcazzo();
+
+        Classedelcazzo.setDadowo(5);
+
+        System.out.println(Classedelcazzo.getDadowo());
+
+        Classedelcazzo.setStriwinga("e621");
+
+        System.out.println(Classedelcazzo.getStriwinga());
+
+        Classedelcazzo.setNuts("In your mouth");
+
+    }
+}
